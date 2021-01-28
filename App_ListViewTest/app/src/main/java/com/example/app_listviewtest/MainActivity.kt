21a59e7613abf899add05f0a74741627ded4c2a7
ALJ00 <armasjose1980@gmail.com>
2021-01-28 08:32:09 +0100

    MainActivity file up to date

--- App_ListViewTest/app/src/main/java/com/example/app_listviewtest/MainActivity.kt
+++ App_ListViewTest/app/src/main/java/com/example/app_listviewtest/MainActivity.kt
@@ -6,16 +6,50 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
+
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
+        var listView:ListView = this.findViewById(R.id.listView)
+
+        //creating one list of data
+        val list = mutableListOf<String>("uno","dos")
+
+        //initialize an array adapter
+        val adapter:ArrayAdapter<String> = ArrayAdapter(this,android.R.layout.simple_dropdown_item_1line,list)
+
+        // attach the array adapter with list view
+        listView.adapter = adapter
+
+        // list view item click listener
+        listView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+            val selectedItem = parent.getItemAtPosition(position)
+
+            Snackbar.make(view, "Selected : $selectedItem", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+
+        }
+
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+
+            //adding to list new item
+            list.add("nuevo")
+
+            // finally, notify the adapter for data set changed
+            adapter.notifyDataSetChanged()
+            
+            Snackbar.make(view, "New item added", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
     }
